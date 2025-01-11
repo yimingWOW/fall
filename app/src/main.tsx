99ca9bc6c65046d3b48872c4cc5155d538261330
yiming <isyiming.zhang@icloud.com>
2025-01-11 20:57:42 +0800

    fix app deploy bug

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { WalletContextProvider } from './WalletContextProvider';
 import './index.css';
-import { Buffer } from 'buffer';
 
-window.Buffer = Buffer;
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WalletContextProvider>
