e824d8ab08bbd048173d6426b7cd258c7d1cea3d
yiming <isyiming.zhang@icloud.com>
2025-02-09 20:52:56 +0800

    update app

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { WalletContextProvider } from './WalletContextProvider';
 import './style/Theme.css';
@@ -9,10 +9,10 @@ import 'crypto-browserify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <WalletContextProvider>
         <App />
       </WalletContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
