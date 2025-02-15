d4a81f8054292f5c3a6268731f5c0f4e28930d91
yiming <isyiming.zhang@icloud.com>
2025-02-15 23:09:06 +0800

    updage guide

--- app/src/components/Guide/Guide.tsx
+++ app/src/components/Guide/Guide.tsx
@@ -11,12 +11,17 @@ export const Guide: FC = () => {
         <section className="section">
           <h2>Prerequisites</h2>
           <div className="step">
-            <h3>1. Get Devnet Token</h3>
+            <h3>1. Enable Devnet in Phantom Wallet</h3>
+            <p>Open Phantom wallet settings → Developer Settings → Open Testnet Mode</p>
+            <img src={require('../../assets/guide_pic_1.png')} alt="Enable Devnet in Phantom" className="guide-image" />
+          </div>
+          <div className="step">
+            <h3>2. Get Devnet Token</h3>
             <p>Obtain Devnet SOL from the <a className="text-link" href="https://faucet.solana.com/" target="_blank" rel="noopener noreferrer">Solana Faucet</a></p>
             <p>Obtain Devnet USDT tokens from the <a className="text-link" href="https://spl-token-faucet.com/?token-name=USDT" target="_blank" rel="noopener noreferrer">SPL Token Faucet</a></p>
           </div>
           <div className="step">
-            <h3>2. Convert SOL to WSOL for trading in Wsol-USDT Pool</h3>
+            <h3>3. Convert SOL to WSOL for trading in Wsol-USDT Pool</h3>
             <WrapSolForm onSuccess={() => {}} />
           </div>
         </section>
